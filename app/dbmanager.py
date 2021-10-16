7f606acf2b613918cdfed73121c2b048fd834cc5
Andrey <mrsun@mail.ru>
2021-10-16 11:20:29 +0300

    module dbmanager

---
+++ app/dbmanager.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+class DBManager:
+    def __init__(self, database='andrey04', host="db", user="root", password="root"):
+        self.connection = mysql.connector.connect(
+            user=user, 
+            password=password,
+            host=host, # name of the mysql service as set in the docker-compose file
+            database=database,
+            auth_plugin='mysql_native_password'
+        )
+        self.cursor = self.connection.cursor()
+    
+    def populate_db(self):
+        self.cursor.execute('DROP TABLE IF EXISTS blog')
+        self.cursor.execute('CREATE TABLE blog (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255))')
+        self.cursor.executemany('INSERT INTO blog (id, title) VALUES (%s, %s);', [(i, 'Blog post #%d'% i) for i in range (1,5)])
+        self.connection.commit()
+    
+    def query_titles(self):
+        self.cursor.execute('SELECT * FROM nomenclature')
+        rec = []
+        for c in self.cursor.fetchall():
+            rec.append(c)
+        return rec
